60b5e5df2e39be9b22545daba7ecbda4116c8618
GabrieliDuarte <gabrieliduarte21@gmail.com>
2026-09-19 18:23:20 -0300

    feat: adiciona seccao de contacto com integracao EmailJS

--- landing-page/src/App.tsx
+++ landing-page/src/App.tsx
@@ -1,6 +1,7 @@
 import Navbar from "./layout/Navbar";
 import Hero from "./layout/Hero";
-import Functionality from "./layout/Functionality";
+import Functionality from "./layout/Functionality"; 
+import Contact from "./layout/Contact";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Navbar />
       <Hero />
       <Functionality />
+      <Contact />
     </>
   );
 }
